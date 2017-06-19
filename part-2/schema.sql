d177653176729c55374c0990aedd629f200c50c8
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-06-19 13:26:02 -0700

    base files and seed data added

---
+++ part-2/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS grocery_store;
+CREATE DATABASE grocery_store;
+
+/c grocery_store
+
+DROP TABLE IF EXISTS grocery_items;
+CREATE TABLE grocery_items(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	price DECIMAL,
+	section VARCHAR(40) NOT NULL
+)
