aed07e1b0f72eb1aa24986fbe4e57cedf0264810
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-06-20 09:25:42 -0700

    separated load-data statements from schema file and updated package json with load-data script

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -11,17 +11,11 @@ CREATE TABLE grocery_items(
 	section VARCHAR(40) NOT NULL
 );
 
-COPY grocery_items(name, price, section) FROM '/Users/kamilalambert/Desktop/Projects/phase-3-challenge/part-2/data/grocery_items.csv' DELIMITER ',' CSV HEADER;
-
+DROP TABLE IF EXISTS shoppers;
 CREATE TABLE shoppers(
 	shopper_id SERIAL PRIMARY KEY,
 	shopper_name VARCHAR(40) NOT NULL
 );
-
-INSERT INTO shoppers (shopper_name) VALUES ('bob');
-INSERT INTO shoppers (shopper_name) VALUES ('megan');
-INSERT INTO shoppers (shopper_name) VALUES ('sean');
-INSERT INTO shoppers (shopper_name) VALUES ('vanessa');
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders(
@@ -30,17 +24,3 @@ CREATE TABLE orders(
 	shopper_id INTEGER NOT NULL,
 	name VARCHAR(40) NOT NULL
 );
-
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (1, '2017-01-01', 1, 'Coffee');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (1, '2017-01-02', 1, 'Flour');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (2, '2017-01-03', 1, 'Ketchup');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (3, '2017-01-04', 2, 'Oranges');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (4, '2017-01-05', 3, 'Pasta');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (4, '2017-01-06', 3, 'Pizza');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (5, '2017-01-07', 4, 'Rice');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (5, '2017-01-08', 4, 'Salami');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (5, '2017-01-09', 4, 'Fish');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
-INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
