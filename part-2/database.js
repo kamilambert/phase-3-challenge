69af3b903d3118605bc24a1e7346cfb60b6f80b5
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-06-19 17:22:00 -0700

    all queries except orderTotal functional

--- part-2/database.js
+++ part-2/database.js
@@ -1,5 +1,28 @@
 const pgp = require("pg-promise")
 
 function allItems() {
-	return db.
+	return db.any('SELECT * FROM grocery_items')
+}
+
+function itemsInSection(section) {
+	return db.any('SELECT (id, name) FROM grocery_items WHERE id=$1', section)
+}
+
+function cheapItems() {
+	return db.any('SELECT (id, price) FROM grocery_items WHERE price < 10 ORDER BY price ASC')
+}
+
+function countItemsInSection(section) {
+	return db.any('SELECT count(*) as "Number of Items" FROM grocery_items WHERE section=$1', section)
+}
+
+function mostRecentOrders() {
+	return db.any('SELECT (order_id, order_date) FROM orders ORDER BY order_date DESC LIMIT 10')
+}
+
+function lastShopperName() {
+	return db.any('SELECT shoppers.shopper_name FROM orders
+		INNER JOIN shoppers ON shoppers.shopper_id=orders.shopper_id
+		ORDER BY order_date
+		DESC LIMIT 1')
 }
--- part-2/schema.sql
+++ part-2/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE grocery_items(
 COPY grocery_items(name, price, section) FROM '/Users/kamilalambert/Desktop/Projects/phase-3-challenge/part-2/data/grocery_items.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE shoppers(
-	id SERIAL PRIMARY KEY,
+	shopper_id SERIAL PRIMARY KEY,
 	shopper_name VARCHAR(40) NOT NULL
 );
 
@@ -25,8 +25,22 @@ INSERT INTO shoppers (shopper_name) VALUES ('vanessa');
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders(
-	id SERIAL PRIMARY KEY,
+	order_id INTEGER,
 	order_date DATE,
-	shopper_name VARCHAR(40) NOT NULL,
+	shopper_id INTEGER NOT NULL,
 	name VARCHAR(40) NOT NULL
 );
+
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (1, '2017-01-01', 1, 'Coffee');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (1, '2017-01-02', 1, 'Flour');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (2, '2017-01-03', 1, 'Ketchup');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (3, '2017-01-04', 2, 'Oranges');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (4, '2017-01-05', 3, 'Pasta');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (4, '2017-01-06', 3, 'Pizza');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (5, '2017-01-07', 4, 'Rice');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (5, '2017-01-08', 4, 'Salami');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (5, '2017-01-09', 4, 'Fish');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
+INSERT INTO orders (order_id, order_date, shopper_id, name) VALUES (6, '2017-01-10', 4, 'Oil');
